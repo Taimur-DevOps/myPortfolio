f5642156b0885132170dc39f432a6f1570580813
taimur1997 <taimur.ahmad@nxb.com.pk>
2024-07-10 17:05:08 +0500

    portoflio links added

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -30,91 +30,93 @@ const Line = styled(motion.span)`
 
 const SocialIcons = (props) => {
   return (
-    <Icons>
-      <motion.div
-        initial={{scale:0 }}
-        animate={{ scale: [0, 1, 1.5, 1] }}
-        transition={{ type: "spring", duration: 1, delay: 1 }}
-      >
-        <a
-          style={{ color: "inherit" }}
-          target="_blank"
-          href={"https://github.com/codebucks27"}
-        >
-          <Github
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
-        </a>
-      </motion.div>
-      <motion.div
-        initial={{scale:0 }}
-        animate={{ scale: [0, 1, 1.5, 1] }}
-        transition={{ type: "spring", duration: 1, delay: 1.2 }}
-      >
-        <a
-          style={{ color: "inherit" }}
-          target="_blank"
-          href={"https://twitter.com/code_bucks"}
-        >
-          <Twitter
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
-        </a>
-      </motion.div>
-      <motion.div
-        initial={{scale:0 }}
-        animate={{ scale: [0, 1, 1.5, 1] }}
-        transition={{ type: "spring", duration: 1, delay: 1.4 }}
-      >
-        <a
-          style={{ color: "inherit" }}
-          target="_blank"
-          href={"https://facebook.com/codebucks27"}
-        >
-          <Facebook
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
-        </a>
-      </motion.div>
-      <motion.div
-        initial={{scale:0 }}
-        animate={{ scale: [0, 1, 1.5, 1] }}
-        transition={{ type: "spring", duration: 1, delay: 1.6 }}
-      >
-        <a
-          style={{ color: "inherit" }}
-          target="_blank"
-          href={"https://youtube.com"}
-        >
-          <YouTube
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
-        </a>
-      </motion.div>
+    // <Icons>
+    //   <motion.div
+    //     initial={{scale:0 }}
+    //     animate={{ scale: [0, 1, 1.5, 1] }}
+    //     transition={{ type: "spring", duration: 1, delay: 1 }}
+    //   >
+    //     <a
+    //       style={{ color: "inherit" }}
+    //       target="_blank"
+    //       href={"https://github.com/codebucks27"}
+    //     >
+    //       <Github
+    //         width={25}
+    //         height={25}
+    //         fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+    //       />
+    //     </a>
+    //   </motion.div>
+    //   <motion.div
+    //     initial={{scale:0 }}
+    //     animate={{ scale: [0, 1, 1.5, 1] }}
+    //     transition={{ type: "spring", duration: 1, delay: 1.2 }}
+    //   >
+    //     <a
+    //       style={{ color: "inherit" }}
+    //       target="_blank"
+    //       href={"https://twitter.com/code_bucks"}
+    //     >
+    //       <Twitter
+    //         width={25}
+    //         height={25}
+    //         fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+    //       />
+    //     </a>
+    //   </motion.div>
+    //   <motion.div
+    //     initial={{scale:0 }}
+    //     animate={{ scale: [0, 1, 1.5, 1] }}
+    //     transition={{ type: "spring", duration: 1, delay: 1.4 }}
+    //   >
+    //     <a
+    //       style={{ color: "inherit" }}
+    //       target="_blank"
+    //       href={"https://facebook.com/codebucks27"}
+    //     >
+    //       <Facebook
+    //         width={25}
+    //         height={25}
+    //         fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+    //       />
+    //     </a>
+    //   </motion.div>
+    //   <motion.div
+    //     initial={{scale:0 }}
+    //     animate={{ scale: [0, 1, 1.5, 1] }}
+    //     transition={{ type: "spring", duration: 1, delay: 1.6 }}
+    //   >
+    //     <a
+    //       style={{ color: "inherit" }}
+    //       target="_blank"
+    //       href={"https://youtube.com"}
+    //     >
+    //       <YouTube
+    //         width={25}
+    //         height={25}
+    //         fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+    //       />
+    //     </a>
+    //   </motion.div>
 
-      <Line
-        color={props.theme}
-        initial={{
-          height: 0,
-        }}
-        animate={{
-          height: "8rem",
-        }}
-        transition={{
-          type: "spring",
-          duration: 1,
-          delay: 0.8,
-        }}
-      />
-    </Icons>
+    //   <Line
+    //     color={props.theme}
+    //     initial={{
+    //       height: 0,
+    //     }}
+    //     animate={{
+    //       height: "8rem",
+    //     }}
+    //     transition={{
+    //       type: "spring",
+    //       duration: 1,
+    //       delay: 0.8,
+    //     }}
+    //   />
+    // </Icons>
+    <>
+    </>
   );
 };
 
